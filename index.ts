af19ba817efa4712928f19e08691ac2995672845
gastonioter <iotting.castro10@gmail.com>
2024-12-06 18:10:29 -0300

    cleaning

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import { runAgent } from './src/agent';
-import { z } from 'zod';
+
 import { tools } from './src/tools';
 const userMessage = process.argv[2];
 
@@ -8,45 +8,4 @@ if (!userMessage) {
   console.error('Please provide a message');
   process.exit(1);
 }
-
-/* *********** */
-// Chat Based
-/* *********** */
-
-// const currentMessage: AIMessage = {
-//   role: 'user',
-//   content: userMessage,
-// }
-
-// const previousContext: AIMessage[] = await getMessages()
-
-// const response = await runLLM({
-//   messages: [...previousContext, currentMessage],
-//   tools: [],
-// })
-
-// addMessages([
-//   ...previousContext,
-//   currentMessage,
-//   { role: 'assistant', content: response.content },
-// ])
-
-// console.log(response.content)
-
-/* *********** */
-// Agents
-/* *********** */
-
-// functoin definition
-
-const weatherToolParameters = z.object({
-  reasoning: z.string().describe('why did you pick this tool?'),
-});
-const weatherTool = {
-  name: 'get_weather',
-  // when to call this tool
-  description:
-    'information about the weather in Argentina. Dont use this for any other city',
-  parameters: weatherToolParameters,
-};
-const response = await runAgent({ userMessage, tools });
+await runAgent({ userMessage, tools });
