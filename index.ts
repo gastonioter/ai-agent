558f0c3a852dffa1c23432d130f4b78baae66661
gastonioter <iotting.castro10@gmail.com>
2024-12-05 21:12:43 -0300

    chat with history

--- index.ts
+++ index.ts
@@ -1,8 +1,29 @@
 import 'dotenv/config'
-
+import { runLLM } from './src/llm'
+import type { AIMessage } from './types'
+import { addMessages, getMessages } from './src/memory'
 const userMessage = process.argv[2]
 
 if (!userMessage) {
   console.error('Please provide a message')
   process.exit(1)
 }
+
+const currentMessage: AIMessage = {
+  role: 'user',
+  content: userMessage,
+}
+
+const previousContext: AIMessage[] = await getMessages()
+
+const response = await runLLM({
+  messages: [...previousContext, currentMessage],
+})
+
+addMessages([
+  ...previousContext,
+  currentMessage,
+  { role: 'assistant', content: response.content },
+])
+
+console.log(response.content)
---
+++ src/memory.ts
@@ -0,0 +1,43 @@
+import { JSONFilePreset } from 'lowdb/node'
+import type { AIMessage } from '../types'
+import { v4 as uuidv4 } from 'uuid'
+import { openai } from './ai'
+
+export type MessageWithMetadata = AIMessage & {
+  id: string
+  createdAt: string
+}
+
+export const addMetadata = (message: AIMessage): MessageWithMetadata => ({
+  ...message,
+  id: uuidv4(),
+  createdAt: new Date().toISOString(),
+})
+
+export const removeMetadata = (message: MessageWithMetadata): AIMessage => {
+  const { id, createdAt, ...messageWithoutMetadata } = message
+  return messageWithoutMetadata
+}
+
+type Data = {
+  messages: MessageWithMetadata[]
+}
+
+const initialData: Data = { messages: [] }
+
+export const getDb = async () => {
+  const db = await JSONFilePreset<Data>('db.json', initialData)
+
+  return db
+}
+
+export const addMessages = async (messages: AIMessage[]) => {
+  const db = await getDb()
+  db.data.messages.push(...messages.map(addMetadata))
+  await db.write()
+}
+
+export const getMessages = async () => {
+  const db = await getDb()
+  return db.data.messages.map(removeMetadata)
+}
